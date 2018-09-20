346885852900c9abe624d61c41d352ec35cb444e
Felix Pfister <mail@felix-pfister.de>
2018-09-20 16:40:10 +0200

    add missing annotation

--- src/FondOfSpryker/Zed/Search/Business/SearchBusinessFactory.php
+++ src/FondOfSpryker/Zed/Search/Business/SearchBusinessFactory.php
@@ -6,6 +6,9 @@ use FondOfSpryker\Client\Search\Provider\IndexClientProvider;
 use FondOfSpryker\Client\Search\Provider\SearchClientProvider;
 use Spryker\Zed\Search\Business\SearchBusinessFactory as BaseSearchBusinessFactory;
 
+/**
+ * @method \FondOfSpryker\Zed\Search\SearchConfig getConfig()
+ */
 class SearchBusinessFactory extends BaseSearchBusinessFactory
 {
     /**
